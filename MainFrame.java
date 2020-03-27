46570f6f761a651a2c11a20e9c2fdf2dac0f0852
costinaioana18 <61825038+costinaioana18@users.noreply.github.com>
2020-03-27 19:26:47 +0200

    Add files via upload

---
+++ MainFrame.java
@@ -0,0 +1,30 @@
+package sample;
+import javax.swing.*;
+import java.awt.*;
+
+
+public class MainFrame extends JFrame{
+    ConfigPanel configPanel;
+    DrawingPanel canvas;
+    ControlPanel controlPanel;
+
+    /**
+     * creates and adds the panels
+     * sets the layout
+     */
+    public MainFrame()
+    {
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        configPanel=new ConfigPanel(this);
+        controlPanel=new ControlPanel(this);
+        canvas=new DrawingPanel(this);
+        getContentPane().setLayout(new BorderLayout());
+        add(configPanel,BorderLayout.NORTH);
+        add(canvas,BorderLayout.CENTER);
+        add(controlPanel,BorderLayout.SOUTH);
+
+        pack();
+
+    }
+}
